60364f4c9cc02e8dd05d2b09b9ffad85da6e9d98
chengl <chengl@info2soft.com>
2019-01-07 13:59:55 +0800

    分组管理测试 case 输出。

--- info2soft/resource/v20181227/BizGroup.py
+++ info2soft/resource/v20181227/BizGroup.py
@@ -22,27 +22,22 @@ class BizGroup (object):
     '''
      * 2 更新
      * 
-     * @body['uuid'] String  必填 节点uuid
      * @param dict body  参数详见 API 手册
      * @return array
      '''
     def modifyBizGroup(self, body):
         
-        url = '{0}/biz_grp/{1}'.format(config.get_default('default_api_host'), body['uuid'])
-        del body['uuid']
+        url = '{0}/biz_grp/{1}'.format(config.get_default('default_api_host'), body['biz_grp']['grp_uuid'])
         res = https._put(url, body, self.auth)
         return res
 
     '''
      * 3 获取单个
      * 
-     * @body['uuid'] String  必填 节点uuid
      * @return array
      '''
     def describeBizGroup(self, body):
-        if body is None or 'uuid' not in body:
-           exit()
-        url = '{0}/biz_grp/{1}'.format(config.get_default('default_api_host'), body['uuid'])
+        url = '{0}/biz_grp/{1}'.format(config.get_default('default_api_host'), body['grp_uuid'])
         
         res = https._get(url, None, self.auth)
         return res
@@ -76,27 +71,22 @@ class BizGroup (object):
     '''
      * 1 更新绑定
      * 
-     * @body['uuid'] String  必填 节点uuid
      * @param dict body  参数详见 API 手册
      * @return array
      '''
     def updateBizGroupBind(self, body):
         
-        url = '{0}/biz_grp/{1}/bind'.format(config.get_default('default_api_host'), body['uuid'])
-        del body['uuid']
+        url = '{0}/biz_grp/{1}/bind'.format(config.get_default('default_api_host'), body['grp_uuid'])
         res = https._put(url, body, self.auth)
         return res
 
     '''
      * 2 获取绑定情况
      * 
-     * @body['uuid'] String  必填 节点uuid
      * @return array
      '''
     def listBizGroupBind(self, body):
-        if body is None or 'uuid' not in body:
-           exit()
-        url = '{0}/biz_grp/{1}/bind'.format(config.get_default('default_api_host'), body['uuid'])
+        url = '{0}/biz_grp/{1}/bind'.format(config.get_default('default_api_host'), body['grp_uuid'])
         
         res = https._get(url, None, self.auth)
         return res
