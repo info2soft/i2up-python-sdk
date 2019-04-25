f0f4de978d2b9024155ae6354bbd9bb4501a9eed
chengl <chengl@info2soft.com>
2019-04-25 15:58:21 +0800

    [IM]：实例代码输出格式化展示。

--- examples/repBackupList.py
+++ examples/repBackupList.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
-import sys
-sys.path.append(r'C:\python_sdk')
+# import sys
+import json
 from info2soft import RepBackup
 from info2soft import Auth
 
+# sys.path.append(r'C:\python_sdk')
 username = 'admin'
 pwd = 'Info1234'
 
@@ -14,11 +15,16 @@ body = {
     'limit': 1,
     'type': 1,
     'page': 1,
-    'search_field': '',
+    'search_field': ''
 }
 repBackup = RepBackup(a)
 ret, info = repBackup.listRepBackup(body)
 if ret is not None:
     print('All is OK')
+    # 格式化输出结果，提升可读性
+    # Format output results to improve readability
+    dumpRet = json.dumps(ret, sort_keys=True, indent=4, separators=(',', ':'))
+    print(dumpRet)
 else:
-    print(info) # error message in info
+    # error message in info
+    print(info)
