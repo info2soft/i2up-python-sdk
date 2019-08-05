0a12c9ceababf2d74876a798f65a768944ad298f
chengl <chengl@info2soft.com>
2019-08-05 16:41:50 +0800

    feat: 高可用检测名称重复

--- info2soft/ha/v20181227/AppHighAvailability.py
+++ info2soft/ha/v20181227/AppHighAvailability.py
@@ -135,5 +135,18 @@ class AppHighAvailability (object):
         res = https._get(url, None, self.auth)
         return res
 
+    '''
+     * 检查HA名称是否重复
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+     '''
+    def haVerifyName(self, body):
+
+        url = '{0}/ha/verify_name'.format(config.get_default('default_api_host'))
+
+        res = https._post(url, body, self.auth)
+        return res
+
 
 
