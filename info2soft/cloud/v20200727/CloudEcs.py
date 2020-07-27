3d0980dc1f531707ce4e45149d3bbfa9f4f5ebc0
chengl <chengl@info2soft.com>
2020-07-27 15:23:33 +0800

    feat: 云备份、云平台、云硬盘、云主机、云端演练接口

---
+++ info2soft/cloud/v20200727/CloudEcs.py
@@ -0,0 +1,137 @@
+
+from info2soft import config
+from info2soft import https
+
+
+class CloudEcs (object):
+    def __init__(self, auth):
+        self.auth = auth
+    '''
+     *  新建
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def createEcs(self, body):
+        
+        url = '{0}/cloud/ecs'.format(config.get_default('default_api_host'))
+        
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     *  列表
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listEcs(self, body):
+        
+        url = '{0}/cloud/ecs'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     *  列表 - 远程登录
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listVncConsole(self, body):
+        
+        url = '{0}/cloud/ecs/vnc_console'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     *  状态
+     * 
+     * @return list
+    '''
+    def listEcsStatus(self, ):
+        
+        url = '{0}/cloud/ecs/status'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, None, self.auth)
+        return res
+
+    '''
+     *  获取空闲挂载点
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def attachPoint(self, body):
+        
+        url = '{0}/cloud/ecs/attach_point'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     *  配置演练
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def configRehearse(self, body):
+        
+        url = '{0}/cloud/ecs/rehearse_conf'.format(config.get_default('default_api_host'))
+        
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     *  演练组 - 列表
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listRehearseGroup(self, body):
+        
+        url = '{0}/cloud/ecs/rehearse_group'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     *  演练组 - 新建/更新
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def createRehearseGroup(self, body):
+        
+        url = '{0}/cloud/ecs/rehearse_group'.format(config.get_default('default_api_host'))
+        
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     *  演练组 - 删除
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def deleteRehearseGroup(self, body):
+        
+        url = '{0}/cloud/ecs/rehearse_group'.format(config.get_default('default_api_host'))
+        
+        res = https._delete(url, body, self.auth)
+        return res
+
+    '''
+     *  演练组 - 单个
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @return list
+    '''
+    def describeRehearseGroup(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/cloud/ecs/rehearse_group/{1}'.format(config.get_default('default_api_host'), uuid)
+        
+        res = https._get(url, None, self.auth)
+        return res
---
+++ info2soft/cloud/v20200727/CloudPlatform.py
@@ -0,0 +1,139 @@
+
+from info2soft import config
+from info2soft import https
+
+
+class CloudPlatform (object):
+    def __init__(self, auth):
+        self.auth = auth
+    '''
+     *  准备 - 获取区域列表
+     * 
+     * @return list
+    '''
+    def listCloudPlatformRegion(self, ):
+        
+        url = '{0}/cloud/platform/region_list'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, None, self.auth)
+        return res
+
+    '''
+     *  注册
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def registerCloudPlatform(self, body):
+        
+        url = '{0}/cloud/platform'.format(config.get_default('default_api_host'))
+        
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     *  修改
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def modifyCloudPlatform(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/cloud/platform/([a-f0-9-] ]){1}'.format(config.get_default('default_api_host'), uuid)
+        
+        res = https._put(url, body, self.auth)
+        return res
+
+    '''
+     *  删除
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def deleteCloudPlatform(self, body):
+        
+        url = '{0}/cloud/platform'.format(config.get_default('default_api_host'))
+        
+        res = https._delete(url, body, self.auth)
+        return res
+
+    '''
+     *  列表
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listCloudPlatform(self, body):
+        
+        url = '{0}/cloud/platform'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     *  获取单个
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @return list
+    '''
+    def describeCloudPlatform(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/cloud/platform/{1}'.format(config.get_default('default_api_host'), uuid)
+        
+        res = https._get(url, None, self.auth)
+        return res
+
+    '''
+     *  同步云主机
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def syncEcs(self, body):
+        
+        url = '{0}/cloud/platform/sync_ecs'.format(config.get_default('default_api_host'))
+        
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     *  同步云硬盘
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def syncVolume(self, body):
+        
+        url = '{0}/cloud/platform/sync_volume'.format(config.get_default('default_api_host'))
+        
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     *  获取规格信息
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listFlavor(self, body):
+        
+        url = '{0}/cloud/platform/flavor_list'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     *  获取关联节点列表
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listRelativeNode(self, body):
+        
+        url = '{0}/cloud/platform/node_list'.format(config.get_default('default_api_host'))
+        
+        res = https._get(url, body, self.auth)
+        return res
