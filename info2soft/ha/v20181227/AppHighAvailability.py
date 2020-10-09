eb5b02a1ab9c8b85a8828aabb447f0abd8a93b9c
wenc <wenc@info2soft.com>
2020-10-09 16:44:33 +0800

    [IM]: 应用高可用 - 高可用/高可用组 接口更新

--- info2soft/ha/v20181227/AppHighAvailability.py
+++ info2soft/ha/v20181227/AppHighAvailability.py
@@ -7,6 +7,18 @@ class AppHighAvailability (object):
     def __init__(self, auth):
         self.auth = auth
     '''
+     * Dashboard - 高可用列表
+     * 
+     * @return list
+    '''
+    def ha(self, body):
+
+        url = '{0}/dashboard/ha'.format(config.get_default('default_api_host'))
+
+        res = https._get(url, None, self.auth)
+        return res
+
+    '''
      * 高可用列表
      * 
      * @param dict body  参数详见 API 手册
@@ -85,6 +97,19 @@ class AppHighAvailability (object):
         return res
 
     '''
+     * 高可用 - 磁盘信息
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def describeVolumeInfo(self, body):
+
+        url = '{0}ha/volume_info'.format(config.get_default('default_api_host'))
+
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
      * 修改高可用
      * 
      * @param dict body  参数详见 API 手册
@@ -148,5 +173,144 @@ class AppHighAvailability (object):
         res = https._post(url, body, self.auth)
         return res
 
+    '''
+     * 高可用组 - 阶段选项
+     * 
+     * @return list
+    '''
+    def listStageOptions(self, body):
+
+        url = '{0}/ha/group/stage_options'.format(config.get_default('default_api_host'))
+
+        res = https._get(url, None, self.auth)
+        return res
+
+    '''
+     * 高可用组-新建
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def createHAGroup(self, body):
+
+        url = '{0}/ha/group'.format(config.get_default('default_api_host'))
+
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 列表
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listHAGroup(self, body):
+
+        url = '{0}/ha/group'.format(config.get_default('default_api_host'))
+
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 删除
+     * 
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def deleteHAGroup(self, body):
+
+        url = '{0}/ha/group'.format(config.get_default('default_api_host'))
+
+        res = https._delete(url, body, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 修改
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def modifyHAGroup(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/ha/group/{1}'.format(config.get_default('default_api_host'), uuid)
+
+        res = https._put(url, body, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 单个详细信息
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @return list
+    '''
+    def describeHAGroup(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/ha/group/{1}'.format(config.get_default('default_api_host'), uuid)
+
+        res = https._get(url, None, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 组强制切换
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def forceSwitchHAGroup(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/ha/group/{1}/task'.format(config.get_default('default_api_host'), uuid)
+
+        res = https._put(url, body, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 切换状态
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def listHASwitchTaskStatus(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/ha/group/{1}/task/status'.format(config.get_default('default_api_host'), uuid)
+
+        res = https._get(url, body, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 操作强制切换任务 - 暂停
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def pauseHAGroupSwitch(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/ha/group/{1}/task'.format(config.get_default('default_api_host'), uuid)
+
+        res = https._post(url, body, self.auth)
+        return res
+
+    '''
+     * 高可用组 - 操作强制切换任务 - 恢复
+     * 
+     * @body['uuid'] String  必填 节点uuid
+     * @param dict $body  参数详见 API 手册
+     * @return list
+    '''
+    def resumeHAGroupSwitch(self, body, uuid):
+        if uuid is None:
+            exit()
+        url = '{0}/ha/group/{1}/task'.format(config.get_default('default_api_host'), uuid)
+
+        res = https._post(url, body, self.auth)
+        return res
 
 
