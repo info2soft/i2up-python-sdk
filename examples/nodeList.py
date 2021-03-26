3d8617d77775dabc66adb59b336629ec22876dee
ganl <ganl@info2soft.com>
2021-03-26 15:46:54 +0800

    [IM]: 增加获取节点列表的示例代码

---
+++ examples/nodeList.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+import json
+import configparser
+import sys
+import os
+
+# sys.path.append(r'C:\Users\xxx\Desktop\i2soft-i2up-python-sdk-master\i2up-python-sdk')
+from info2soft import Auth
+from info2soft.resource.v20181227 import Node
+
+# conf = configparser.ConfigParser()
+# conf.read('C:\\Users\\Stone\\Desktop\\i2soft-i2up-python-sdk-develop\\i2up-python-sdk\\config.ini', encoding="utf-8-sig")
+# appTable = json.loads(conf.get('appTable', 'value'))
+# print(appTable)
+
+username = 'admin'
+pwd = 'Info1234'
+
+auth = Auth(username, pwd)
+nodebody = {
+    # 'limit': 1000,
+    # 'page': 1,
+}
+
+node = Node(auth)
+
+noderes, info = node.listNode(nodebody)
+if noderes is not None:
+    print('All is OK')
+    # 格式化输出结果，提升可读性
+    # Format output results to improve readability
+    dumpRet = json.dumps(noderes, sort_keys=True, indent=4, separators=(',', ':'))
+    print(dumpRet)
+else:
+    # error message in info
+    print(info)
